83b42cd1bdc2e21e37a2ccf50df72b045f3b6eb9
JAYASOORYA M R <JAYASOORYA>
2019-07-28 18:43:05 +0530

    Updates

---
+++ precache-manifest.219b02638c52410f8fcf9cc56045a83a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bc25445060bfb8c83e363727be4620a",
+    "url": "/jayasoorya_resume/index.html"
+  },
+  {
+    "revision": "0e9b1a22dae5ba8a9b87",
+    "url": "/jayasoorya_resume/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/jayasoorya_resume/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "0e9b1a22dae5ba8a9b87",
+    "url": "/jayasoorya_resume/static/js/main.52f61194.chunk.js"
+  },
+  {
+    "revision": "3eab6e7fb77ecd368f73",
+    "url": "/jayasoorya_resume/static/js/runtime~main.65b41f80.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jayasoorya_resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/jayasoorya_resume/precache-manifest.219b02638c52410f8fcf9cc56045a83a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jayasoorya_resume/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
